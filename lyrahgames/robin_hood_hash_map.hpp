5ab81fa1aa2a2a18320d2192cdd5eb6cf7f46c79
lyrahgames <lyrahgames@mailbox.org>
2020-09-10 21:34:48 +0200

    Templatize Robin Hood hash map

--- lyrahgames/robin_hood_hash_map.hpp
+++ lyrahgames/robin_hood_hash_map.hpp
@@ -2,25 +2,28 @@
 #include <functional>
 #include <vector>
 
-namespace lyrahgames {
+namespace lyrahgames::robin_hood {
 
-class robin_hood_hash_map {
+template <typename Key, typename T, typename Hash = std::hash<Key>,
+          typename Key_equal = std::equal_to<Key>>
+class hash_map {
  public:
-  using key_type = int;
-  using mapped_type = int;
-  using flag_type = size_t;
+  using key_type = Key;
+  using mapped_type = T;
+  using hasher = Hash;
+  using key_equal = Key_equal;
 
   struct container {
     container(size_t n = 0) : size{n} {
       keys = new key_type[size];
       values = new mapped_type[size];
-      flags = new flag_type[size]{0};
+      psl = new size_t[size]{0};
     }
 
     ~container() {
       delete[] keys;
       delete[] values;
-      delete[] flags;
+      delete[] psl;
     }
 
     container(const container&) = delete;
@@ -31,19 +34,17 @@ class robin_hood_hash_map {
     void swap(container& c) noexcept {
       std::swap(keys, c.keys);
       std::swap(values, c.values);
-      std::swap(flags, c.flags);
+      std::swap(psl, c.psl);
       std::swap(size, c.size);
     }
 
     key_type* keys{};
     mapped_type* values{};
-    flag_type* flags{};
+    size_t* psl{};
     size_t size{};
   };
 
-  robin_hood_hash_map() = default;
-
-  static constexpr size_t hash(key_type key) noexcept { return key; }
+  hash_map() = default;
 
   bool empty() const noexcept { return _load == 0; }
   size_t size() const noexcept { return _load; }
@@ -52,7 +53,7 @@ class robin_hood_hash_map {
   std::pair<size_t, size_t> key_swap_index(const key_type& key) const noexcept {
     size_t index = hash(key) & _capacity_mask;
     size_t psl = 1;
-    for (; psl <= _table.flags[index]; ++psl) {
+    for (; psl <= _table.psl[index]; ++psl) {
       if (_table.keys[index] == key) return {index, psl};
       index = (index + 1) & _capacity_mask;
     }
@@ -63,17 +64,17 @@ class robin_hood_hash_map {
       const key_type& key) const noexcept {
     size_t index = hash(key) & _capacity_mask;
     size_t psl = 1;
-    for (; psl <= _table.flags[index]; ++psl) {
+    for (; psl <= _table.psl[index]; ++psl) {
       index = (index + 1) & _capacity_mask;
     }
     return {index, psl};
   }
 
   void insert_key_by_swap(const key_type& key, size_t index, size_t psl) {
-    if (!_table.flags[index]) {
+    if (!_table.psl[index]) {
       _table.keys[index] = key;
       // _table.values[index] = std::move(tmp_value);
-      _table.flags[index] = psl;
+      _table.psl[index] = psl;
       return;
     }
 
@@ -81,13 +82,13 @@ class robin_hood_hash_map {
     mapped_type tmp_value = std::move(_table.values[index]);
     _table.keys[index] = key;
     // _table.values[index] = mapped_type{};
-    std::swap(psl, _table.flags[index]);
+    std::swap(psl, _table.psl[index]);
     ++psl;
     index = (index + 1) & _capacity_mask;
 
-    for (; _table.flags[index]; ++psl) {
-      if (psl > _table.flags[index]) {
-        std::swap(psl, _table.flags[index]);
+    for (; _table.psl[index]; ++psl) {
+      if (psl > _table.psl[index]) {
+        std::swap(psl, _table.psl[index]);
         std::swap(tmp_key, _table.keys[index]);
         std::swap(tmp_value, _table.values[index]);
       }
@@ -96,7 +97,7 @@ class robin_hood_hash_map {
 
     _table.keys[index] = std::move(tmp_key);
     _table.values[index] = std::move(tmp_value);
-    _table.flags[index] = psl;
+    _table.psl[index] = psl;
   }
 
   void double_capacity_and_rehash() {
@@ -104,7 +105,7 @@ class robin_hood_hash_map {
     _table.swap(old_table);
     _capacity_mask = _table.size - 1u;
     for (size_t i = 0; i < old_table.size; ++i) {
-      if (old_table.flags[i]) {
+      if (old_table.psl[i]) {
         const auto [index, psl] = new_key_swap_index(old_table.keys[i]);
         insert_key_by_swap(old_table.keys[i], index, psl);
         _table.values[index] = old_table.values[i];
@@ -116,7 +117,7 @@ class robin_hood_hash_map {
     // Try to find the element.
     size_t index = hash(key) & _capacity_mask;
     size_t psl = 1;
-    for (; psl <= _table.flags[index]; ++psl) {
+    for (; psl <= _table.psl[index]; ++psl) {
       if (_table.keys[index] == key) return _table.values[index];
       index = (index + 1) & _capacity_mask;
     }
@@ -137,7 +138,7 @@ class robin_hood_hash_map {
   mapped_type* find(const key_type& key) const noexcept {
     size_t index = hash(key) & _capacity_mask;
     size_t psl = 1;
-    for (; psl <= _table.flags[index]; ++psl) {
+    for (; psl <= _table.psl[index]; ++psl) {
       if (_table.keys[index] == key) return &_table.values[index];
       index = (index + 1) & _capacity_mask;
     }
@@ -146,21 +147,21 @@ class robin_hood_hash_map {
 
   void erase_by_swap(size_t index) {
     size_t next_index = (index + 1) & _capacity_mask;
-    while (_table.flags[next_index] > 1) {
+    while (_table.psl[next_index] > 1) {
       _table.keys[index] = std::move(_table.keys[next_index]);
       _table.values[index] = std::move(_table.values[next_index]);
-      _table.flags[index] = _table.flags[next_index] - 1;
+      _table.psl[index] = _table.psl[next_index] - 1;
       index = next_index;
       next_index = (next_index + 1) & _capacity_mask;
     }
-    _table.flags[index] = 0;
+    _table.psl[index] = 0;
     // destroy key and value
   }
 
   bool erase(const key_type& key) noexcept {
     size_t index = hash(key) & _capacity_mask;
     size_t psl = 1;
-    for (; psl <= _table.flags[index]; ++psl) {
+    for (; psl <= _table.psl[index]; ++psl) {
       if (_table.keys[index] == key) {
         erase_by_swap(index);
         --_load;
@@ -172,9 +173,12 @@ class robin_hood_hash_map {
   }
 
   container _table{8};
+  hasher hash{};
+  key_equal equal_to{};
+
   size_t _load{};
   size_t _capacity_mask{7};
   float _max_load_factor{0.8};
 };
 
-}  // namespace lyrahgames
+}  // namespace lyrahgames::robin_hood
--- tests/basics/robin_hood_hash_map.cpp
+++ tests/basics/robin_hood_hash_map.cpp
@@ -2,64 +2,86 @@
 #include <iomanip>
 #include <iostream>
 #include <lyrahgames/robin_hood_hash_map.hpp>
+#include <string>
 
 using namespace std;
 
-ostream& operator<<(ostream& os, lyrahgames::robin_hood_hash_map& map) {
+template <typename Key, typename T, typename Hash, typename Key_equal>
+ostream& operator<<(
+    ostream& os,
+    lyrahgames::robin_hood::hash_map<Key, T, Hash, Key_equal>& map) {
   for (size_t i = 0; i < map._table.size; ++i) {
-    os << setw(5) << i << ":" << setw(5) << map._table.keys[i] << " ->"
+    os << setw(5) << i << ":" << setw(8) << map._table.keys[i] << " ->"
        << setw(5) << map._table.values[i] << " :" << setw(5)
-       << (int)map._table.flags[i] << '\n';
+       << (int)map._table.psl[i] << '\n';
   }
   return os;
 }
 
+template <typename Key>
+struct trivial_hash {
+  constexpr size_t operator()(const Key& key) const noexcept {
+    return static_cast<size_t>(key);
+  }
+};
+
 int main() {
-  lyrahgames::robin_hood_hash_map map{};
+  using lyrahgames::robin_hood::hash_map;
 
-  // assert(map.size() == 0);
-  // assert(map.capacity() == 8);
-  // assert(map.empty());
-  // assert(map.find(5) == nullptr);
+  {
+    hash_map<int, int, trivial_hash<int>> map{};
+    cout << map << '\n';
+    map[1] = 1;
+    cout << map << '\n';
+    map[9] = 2;
+    cout << map << '\n';
+    map[16] = 3;
+    cout << map << '\n';
+    map[2] = 4;
+    cout << map << '\n';
+    map[24] = 5;
+    cout << map << '\n';
+    map[1] = 6;
+    cout << map << '\n';
+    map[6] = 8;
+    cout << map << '\n';
+    // map[32] = 7;
+    // cout << map << '\n';
+    map.erase(1);
+    cout << map << '\n';
+  }
 
-  // map.insert(0, 1);
-
-  // assert(map.size() == 1);
-  // assert(!map.empty());
-  // assert(*map.find(0) == 1);
-  // assert(map.find(4) == nullptr);
-
-  // map.insert(3, 2);
-  // map.insert(7, 3);
-  // map.insert(24, 4);
-  // map.insert(4, 5);
-  // map.insert(8, 6);
-  // map.insert(8, 7);
-  // map.insert(123, 8);
-  // map[24] = 4;
-  // map[123] = 8;
-  // map[8] = 6;
-  // map[3] = 2;
-  // map[5] = 5;
-  // map[10] = 1;
-
-  cout << map << '\n';
-  map[1] = 1;
-  cout << map << '\n';
-  map[9] = 2;
-  cout << map << '\n';
-  map[16] = 3;
-  cout << map << '\n';
-  map[2] = 4;
-  cout << map << '\n';
-  map[24] = 5;
-  cout << map << '\n';
-  map[1] = 6;
-  cout << map << '\n';
-  map[6] = 8;
-  cout << map << '\n';
-  map[32] = 7;
-  cout << map << '\n';
-  map.erase(1);
-  cout << map << '\n';
+  {
+    hash_map<std::string, int> map{};
+    cout << map << '\n';
+    map["test"] = 1;
+    map["helo"] = 2;
+    map["cd"] = 3;
+    map["cp"] = 4;
+    map["ls"] = 5;
+    map["tree"] = 6;
+    map["cat"] = 7;
+    map["mkdir"] = 8;
+    map["rm"] = 9;
+    map["ls"] = 10;
+    map["b"] = 11;
+    map["bdep"] = 12;
+    map["g++"] = 13;
+    map["clang"] = 14;
+    map["make"] = 15;
+    map["bpkg"] = 16;
+    map["bash"] = 17;
+    map["fish"] = 18;
+    map["top"] = 19;
+    map["htop"] = 20;
+    map["git"] = 21;
+    map["vim"] = 22;
+    map["touch"] = 23;
+    map["rmdir"] = 24;
+    map["sudo"] = 25;
+    map["nano"] = 26;
+    cout << map << '\n';
+    map.erase("bpkg");
+    cout << map << '\n';
+  }
 }
