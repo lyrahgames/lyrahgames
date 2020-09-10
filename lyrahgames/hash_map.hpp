e4b915dfbf6b0db6284d9ae663a3d6592fad9a03
lyrahgames <lyrahgames@mailbox.org>
2020-09-10 14:52:26 +0200

    Add lazy deletion to hash map routines

--- lyrahgames/hash_map.hpp
+++ lyrahgames/hash_map.hpp
@@ -50,6 +50,8 @@ class hash_map {
   // const_iterator end() const noexcept;
   // const_iterator cend() const noexcept;
 
+  static constexpr size_t hash(key_type key) noexcept { return key; }
+
   bool empty() const noexcept { return _load == 0; }
   size_t size() const noexcept { return _load; }
   size_t capacity() const noexcept { return _table.size; }
@@ -66,7 +68,7 @@ class hash_map {
 
   size_t key_index(key_type key) const noexcept {
     const auto capacity_mask = _table.size - 1u;
-    auto index = std::hash<key_type>{}(key)&capacity_mask;
+    auto index = hash(key) & capacity_mask;
     while ((_table.flags[index]) && (_table.keys[index] != key))
       index = (index + 1) & capacity_mask;
     return index;
@@ -74,13 +76,15 @@ class hash_map {
 
   size_t key_insert_index(key_type key) const noexcept {
     const auto capacity_mask = _table.size - 1u;
-    auto index = std::hash<key_type>{}(key)&capacity_mask;
-    auto deleted_index = index;
+    size_t index = hash(key) & capacity_mask;
+    size_t deleted_index = -1u;
     while ((_table.flags[index]) && (_table.keys[index] != key)) {
-      if (_table.flags[index] == 2) deleted_index = index;
+      if ((deleted_index == -1u) && (_table.flags[index] == 2))
+        deleted_index = index;
       index = (index + 1) & capacity_mask;
     }
-    return index;
+    return (!_table.flags[index] && (deleted_index != -1u)) ? (deleted_index)
+                                                            : (index);
   }
 
   // void rehash(size_t n) {}
@@ -102,31 +106,39 @@ class hash_map {
   // float max_load_factor() const noexcept;
   // void max_load_factor(float m);
 
-  void insert(std::pair<key_type, mapped_type> n) {
-    const auto index = key_index(n.first);
-    if (!_table.flags[index]) ++_load;
-    _table.keys[index] = n.first;
-    _table.values[index] = n.second;
-    _table.flags[index] = 1;
-    if (_load >= _max_load_factor * _table.size) double_capacity_and_rehash();
-  }
+  // void insert(std::pair<key_type, mapped_type> n) {
+  //   const auto index = key_index(n.first);
+  //   if (!_table.flags[index]) ++_load;
+  //   _table.keys[index] = n.first;
+  //   _table.values[index] = n.second;
+  //   _table.flags[index] = 1;
+  //   if (_load >= _max_load_factor * _table.size)
+  //   double_capacity_and_rehash();
+  // }
 
-  void insert(const key_type& key, const mapped_type& value) {
-    const auto index = key_index(key);
-    if (!_table.flags[index]) ++_load;
-    _table.keys[index] = key;
-    _table.values[index] = value;
-    _table.flags[index] = 1;
-    if (_load >= _max_load_factor * _table.size) double_capacity_and_rehash();
-  }
+  // void insert(const key_type& key, const mapped_type& value) {
+  //   const auto index = key_insert_index(key);
+  //   if (_table.flags[index] != 1) {
+  //     ++_load;
+  //     if (_table.flags[index] == 2) --_tombstones;
+  //     _table.keys[index] = key;
+  //     _table.flags[index] = 1;
+  //     if (_load >= _max_load_factor * _table.size)
+  //     double_capacity_and_rehash();
+  //   }
+  //   _table.values[index] = value;
+  // }
 
   mapped_type& operator[](const key_type& key) {
-    const auto index = key_index(key);
-    if (!_table.flags[index]) {
+    const auto index = key_insert_index(key);
+    if (_table.flags[index] != 1) {
       ++_load;
+      _tombstones = _tombstones - (_table.flags[index] == 2);
+
       _table.keys[index] = key;
       // _table.values[index] = {};
       _table.flags[index] = 1;
+
       if (_load >= _max_load_factor * _table.size) {
         double_capacity_and_rehash();
         // The index has been invalidated by rehashing.
@@ -145,11 +157,17 @@ class hash_map {
   bool erase(const key_type& key) {
     const auto index = key_index(key);
     if (!_table.flags[index]) return false;
+    _table.keys[index] = -1;
+    _table.values[index] = -1;
     _table.flags[index] = 2;
+    ++_tombstones;
+    --_load;
+    return true;
   }
 
   container _table{8};
   size_t _load{};
+  size_t _tombstones{};
   float _max_load_factor{0.8};
 };
 
--- tests/basics/hash_map.cpp
+++ tests/basics/hash_map.cpp
@@ -7,8 +7,8 @@ using namespace std;
 
 ostream& operator<<(ostream& os, lyrahgames::hash_map& map) {
   for (size_t i = 0; i < map._table.size; ++i) {
-    os << setw(5) << map._table.keys[i] << ": " << map._table.values[i] << ": "
-       << (int)map._table.flags[i] << '\n';
+    os << setw(5) << map._table.keys[i] << ":" << setw(5)
+       << map._table.values[i] << ": " << (int)map._table.flags[i] << '\n';
   }
   return os;
 }
@@ -16,30 +16,58 @@ ostream& operator<<(ostream& os, lyrahgames::hash_map& map) {
 int main() {
   lyrahgames::hash_map map{};
 
-  assert(map.size() == 0);
-  assert(map.capacity() == 8);
-  assert(map.empty());
-  assert(map.find(5) == nullptr);
+  // assert(map.size() == 0);
+  // assert(map.capacity() == 8);
+  // assert(map.empty());
+  // assert(map.find(5) == nullptr);
 
-  map.insert(0, 1);
+  // map.insert(0, 1);
 
-  assert(map.size() == 1);
-  assert(!map.empty());
-  assert(*map.find(0) == 1);
-  assert(map.find(4) == nullptr);
+  // assert(map.size() == 1);
+  // assert(!map.empty());
+  // assert(*map.find(0) == 1);
+  // assert(map.find(4) == nullptr);
 
-  map.insert(3, 2);
-  map.insert(7, 3);
-  map.insert(24, 4);
-  map.insert(4, 5);
-  map.insert(8, 6);
-  map.insert(8, 7);
-  map.insert(123, 8);
+  // map.insert(3, 2);
+  // map.insert(7, 3);
+  // map.insert(24, 4);
+  // map.insert(4, 5);
+  // map.insert(8, 6);
+  // map.insert(8, 7);
+  // map.insert(123, 8);
   // map[24] = 4;
   // map[123] = 8;
   // map[8] = 6;
   // map[3] = 2;
   // map[5] = 5;
   // map[10] = 1;
+
+  cout << map << '\n';
+  map[0] = 1;
+  cout << map << '\n';
+  map[8] = 2;
+  cout << map << '\n';
+  map[16] = 3;
+  cout << map << '\n';
+  map[1] = 4;
+  cout << map << '\n';
+  map.erase(16);
+  cout << map << '\n';
+  assert(map.find(16) == nullptr);
+  assert(*map.find(1) == 4);
+  assert(map._tombstones == 1);
+  map[2] = 5;
+  cout << map << '\n';
+  assert(map._tombstones == 0);
+  map.erase(8);
+  cout << map << '\n';
+  map[1] = 6;
+  cout << map << '\n';
+  map.erase(2);
+  assert(map._tombstones == 2);
+  cout << map << '\n';
+  map[1] = 7;
+  cout << map << '\n';
+  map[10] = 8;
   cout << map << '\n';
 }
