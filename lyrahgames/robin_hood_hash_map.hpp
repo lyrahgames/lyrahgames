2314ea28e5afffc6bc2d3047994dd29e2b2bcc16
lyrahgames <lyrahgames@mailbox.org>
2020-09-10 18:45:56 +0200

    Start with hash map based on Robin Hood hashing

---
+++ lyrahgames/robin_hood_hash_map.hpp
@@ -0,0 +1,182 @@
+#pragma once
+#include <functional>
+#include <vector>
+
+namespace lyrahgames {
+
+class robin_hood_hash_map {
+ public:
+  using key_type = int;
+  using mapped_type = int;
+  using flag_type = size_t;
+
+  struct container {
+    container(size_t n = 0) : size{n} {
+      keys = new key_type[size];
+      values = new mapped_type[size];
+      flags = new flag_type[size]{0};
+    }
+
+    ~container() {
+      delete[] keys;
+      delete[] values;
+      delete[] flags;
+    }
+
+    container(const container&) = delete;
+    container& operator=(const container&) = delete;
+    container(container&&) = delete;
+    container& operator=(container&&) = delete;
+
+    void swap(container& c) noexcept {
+      std::swap(keys, c.keys);
+      std::swap(values, c.values);
+      std::swap(flags, c.flags);
+      std::swap(size, c.size);
+    }
+
+    key_type* keys{};
+    mapped_type* values{};
+    flag_type* flags{};
+    size_t size{};
+  };
+
+  robin_hood_hash_map() = default;
+
+  static constexpr size_t hash(key_type key) noexcept { return key; }
+
+  bool empty() const noexcept { return _load == 0; }
+  size_t size() const noexcept { return _load; }
+  size_t capacity() const noexcept { return _table.size; }
+
+  std::pair<size_t, size_t> key_swap_index(const key_type& key) const noexcept {
+    size_t index = hash(key) & _capacity_mask;
+    size_t psl = 1;
+    for (; psl <= _table.flags[index]; ++psl) {
+      if (_table.keys[index] == key) return {index, psl};
+      index = (index + 1) & _capacity_mask;
+    }
+    return {index, psl};
+  }
+
+  std::pair<size_t, size_t> new_key_swap_index(
+      const key_type& key) const noexcept {
+    size_t index = hash(key) & _capacity_mask;
+    size_t psl = 1;
+    for (; psl <= _table.flags[index]; ++psl) {
+      index = (index + 1) & _capacity_mask;
+    }
+    return {index, psl};
+  }
+
+  void insert_key_by_swap(const key_type& key, size_t index, size_t psl) {
+    if (!_table.flags[index]) {
+      _table.keys[index] = key;
+      // _table.values[index] = std::move(tmp_value);
+      _table.flags[index] = psl;
+      return;
+    }
+
+    key_type tmp_key = std::move(_table.keys[index]);
+    mapped_type tmp_value = std::move(_table.values[index]);
+    _table.keys[index] = key;
+    // _table.values[index] = mapped_type{};
+    std::swap(psl, _table.flags[index]);
+    ++psl;
+    index = (index + 1) & _capacity_mask;
+
+    for (; _table.flags[index]; ++psl) {
+      if (psl > _table.flags[index]) {
+        std::swap(psl, _table.flags[index]);
+        std::swap(tmp_key, _table.keys[index]);
+        std::swap(tmp_value, _table.values[index]);
+      }
+      index = (index + 1) & _capacity_mask;
+    }
+
+    _table.keys[index] = std::move(tmp_key);
+    _table.values[index] = std::move(tmp_value);
+    _table.flags[index] = psl;
+  }
+
+  void double_capacity_and_rehash() {
+    container old_table{_table.size << 1u};
+    _table.swap(old_table);
+    _capacity_mask = _table.size - 1u;
+    for (size_t i = 0; i < old_table.size; ++i) {
+      if (old_table.flags[i]) {
+        const auto [index, psl] = new_key_swap_index(old_table.keys[i]);
+        insert_key_by_swap(old_table.keys[i], index, psl);
+        _table.values[index] = old_table.values[i];
+      }
+    }
+  }
+
+  mapped_type& operator[](const key_type& key) {
+    size_t index = hash(key) & _capacity_mask;
+
+    // Try to find the element.
+    size_t psl = 1;
+    for (; psl <= _table.flags[index]; ++psl) {
+      if (_table.keys[index] == key) return _table.values[index];
+      index = (index + 1) & _capacity_mask;
+    }
+
+    // Could not find the element. So insert it by Robin-Hood swaping.
+    ++_load;
+    if (_load >= _max_load_factor * _table.size) {
+      double_capacity_and_rehash();
+      // The index and psl have been invalidated by rehashing.
+      // size_t index = hash(key) & _capacity_mask;
+      // size_t psl = 1;
+      // for (; psl <= _table.flags[index]; ++psl) {
+      //   // Key is not part of map.
+      //   // if (_table.keys[index] == key) break;
+      //   index = (index + 1) & _capacity_mask;
+      // }
+      const auto [tmp_index, tmp_psl] = new_key_swap_index(key);
+      index = tmp_index;
+      psl = tmp_psl;
+    }
+
+    mapped_type& result = _table.values[index];
+
+    insert_key_by_swap(key, index, psl);
+    // if (!_table.flags[index]) {
+    //   _table.keys[index] = key;
+    //   // _table.values[index] = std::move(tmp_value);
+    //   _table.flags[index] = psl;
+    //   return result;
+    // }
+
+    // key_type tmp_key = std::move(_table.keys[index]);
+    // mapped_type tmp_value = std::move(_table.values[index]);
+    // _table.keys[index] = key;
+    // // _table.values[index] = mapped_type{};
+    // std::swap(psl, _table.flags[index]);
+    // ++psl;
+    // index = (index + 1) & _capacity_mask;
+
+    // for (; _table.flags[index]; ++psl) {
+    //   if (psl > _table.flags[index]) {
+    //     std::swap(psl, _table.flags[index]);
+    //     std::swap(tmp_key, _table.keys[index]);
+    //     std::swap(tmp_value, _table.values[index]);
+    //   }
+    //   index = (index + 1) & _capacity_mask;
+    // }
+
+    // _table.keys[index] = std::move(tmp_key);
+    // _table.values[index] = std::move(tmp_value);
+    // _table.flags[index] = psl;
+
+    return result;
+  }
+
+  container _table{8};
+  size_t _load{};
+  size_t _capacity_mask{7};
+  float _max_load_factor{0.8};
+};
+
+}  // namespace lyrahgames
---
+++ tests/basics/robin_hood_hash_map.cpp
@@ -0,0 +1,63 @@
+#include <cassert>
+#include <iomanip>
+#include <iostream>
+#include <lyrahgames/robin_hood_hash_map.hpp>
+
+using namespace std;
+
+ostream& operator<<(ostream& os, lyrahgames::robin_hood_hash_map& map) {
+  for (size_t i = 0; i < map._table.size; ++i) {
+    os << setw(5) << i << ":" << setw(5) << map._table.keys[i] << " ->"
+       << setw(5) << map._table.values[i] << " :" << setw(5)
+       << (int)map._table.flags[i] << '\n';
+  }
+  return os;
+}
+
+int main() {
+  lyrahgames::robin_hood_hash_map map{};
+
+  // assert(map.size() == 0);
+  // assert(map.capacity() == 8);
+  // assert(map.empty());
+  // assert(map.find(5) == nullptr);
+
+  // map.insert(0, 1);
+
+  // assert(map.size() == 1);
+  // assert(!map.empty());
+  // assert(*map.find(0) == 1);
+  // assert(map.find(4) == nullptr);
+
+  // map.insert(3, 2);
+  // map.insert(7, 3);
+  // map.insert(24, 4);
+  // map.insert(4, 5);
+  // map.insert(8, 6);
+  // map.insert(8, 7);
+  // map.insert(123, 8);
+  // map[24] = 4;
+  // map[123] = 8;
+  // map[8] = 6;
+  // map[3] = 2;
+  // map[5] = 5;
+  // map[10] = 1;
+
+  cout << map << '\n';
+  map[1] = 1;
+  cout << map << '\n';
+  map[9] = 2;
+  cout << map << '\n';
+  map[16] = 3;
+  cout << map << '\n';
+  map[2] = 4;
+  cout << map << '\n';
+  map[24] = 5;
+  cout << map << '\n';
+  map[1] = 6;
+  cout << map << '\n';
+  map[32] = 7;
+  cout << map << '\n';
+  map[6] = 8;
+  cout << map << '\n';
+}
